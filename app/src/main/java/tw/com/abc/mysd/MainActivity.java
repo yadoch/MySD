1202cb7d54ca2986b6c7a1fc35dc410c5500e16f
geoff <yadoch@gmail.com>
2017-08-07 21:34:15 +0800

    My SD 未完成

--- app/src/main/java/tw/com/abc/mysd/MainActivity.java
+++ app/src/main/java/tw/com/abc/mysd/MainActivity.java
@@ -1,13 +1,63 @@
 package tw.com.abc.mysd;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity {
+    private boolean isPermissionOK;
+    private File sdroot,approot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // no
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else {
+            isPermissionOK = true;
+            init();
+        }
+
+    }
+
+    private void init(){
+        if (!isPermissionOK) {
+            finish();
+        }else{
+            go();
+        }
+        //Log.i("brad", "start");
+    }
+
+    private void  go(){
+        sdroot= Environment.getExternalStorageDirectory();
+        approot=new File(sdroot,"Android/data/")
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                isPermissionOK = true;
+
+            }
+            init();
+        }
     }
 }
